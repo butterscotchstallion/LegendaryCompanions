4953d100f68f5998b86695f6aabce97eabbe55a2
Butterscotch Stallion <mistborn+github@fastmail.com>
2023-12-01 00:30:26 -0500

    Fixes meta and updates item combo with new ideas

--- LegendaryCompanions/Mods/LegendaryCompanions/ScriptExtender/Lua/Server/Main.lua
+++ LegendaryCompanions/Mods/LegendaryCompanions/ScriptExtender/Lua/Server/Main.lua
@@ -10,9 +10,8 @@ Ext.Require('Server/LegendaryCompanions.lua')
 local function print_start_up_message()
     local mod = Ext.Mod.GetMod(ModuleUUID)
     local version = mod.Info.ModVersion
-    local version_msg = string.format('v%s.%s.%s', version[1], version[2], version[3])
+    local version_msg = string.format('LegendaryCompanions v%s.%s.%s', version[1], version[2], version[3])
     MuffinLogger.info(version_msg)
-    MuffinLogger.critical('SOMETHING CHANGED')
 end
 
 local function on_session_loaded()
