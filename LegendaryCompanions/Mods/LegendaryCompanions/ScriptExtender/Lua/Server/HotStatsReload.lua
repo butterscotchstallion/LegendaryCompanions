1068548f7a7d563911e41f21df1b79d7bb8d4336
Butterscotch Stallion <mistborn+github@fastmail.com>
2023-12-08 22:19:49 -0500

    Fixes equipment file by naming it properly!

--- LegendaryCompanions/Mods/LegendaryCompanions/ScriptExtender/Lua/Server/HotStatsReload.lua
+++ LegendaryCompanions/Mods/LegendaryCompanions/ScriptExtender/Lua/Server/HotStatsReload.lua
@@ -1,11 +1,9 @@
 local MOD_PATH = 'Public/LegendaryCompanions/Stats/Generated/Data/'
 local FILES_TO_RELOAD = {
     'LC_Summons_Buffs.txt',
-    'LC_Githzerai_Equipment.txt',
     'LC_Summons.txt',
     'LC_Books.txt',
 }
-local isGameLoaded = false
 
 local function OnReset()
     for _, filename in pairs(FILES_TO_RELOAD) do
