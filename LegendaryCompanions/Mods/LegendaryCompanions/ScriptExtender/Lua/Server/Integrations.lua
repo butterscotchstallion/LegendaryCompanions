c1190fe7ec65b08f230a7bc2401f0c00a88690f7
Butterscotch Stallion <mistborn+github@fastmail.com>
2023-12-09 23:07:31 -0500

    Updates icon, adds documentation to config, fixes blood surface when new companion summoned

--- LegendaryCompanions/Mods/LegendaryCompanions/ScriptExtender/Lua/Server/Integrations.lua
+++ LegendaryCompanions/Mods/LegendaryCompanions/ScriptExtender/Lua/Server/Integrations.lua
@@ -9,82 +9,40 @@
 local GTY_LEGENDARY_UUID    = '4973f064-fe29-4857-89fb-a18af7a5d02a'
 local GTY_RARE_UUID         = 'bbcf8a7b-5630-4ddf-be17-b361d2289218'
 local GTY_COMMON_UUID       = 'abc73768-6f98-423d-9bf2-7748e1f93b0e'
-local RARITY_COMMON         = 'common'
-local RARITY_RARE           = 'rare'
-local RARITY_LEGENDARY      = 'legendary'
 local DEFAULT_SUMMON_MESAGE = 'The book glows with power and opens a portal!'
+LC['integrations']          = {}
 
-LC['rarities']              = { RARITY_COMMON, RARITY_RARE, RARITY_LEGENDARY }
-LC['integrations']          = {
+LC['integrations'].insert({
+    -- This is the name of your mod
     ['Githzerai'] = {
+        -- A list of lore books
         ['books'] = {
             {
-                ['rarity']          = RARITY_LEGENDARY,
+                -- Name of this book item from the root template
+                ['name']            = 'BOOK_LC_Githzerai_Combined_Tome_3',
+                -- Names of the pages that when combined create the above book
+                ['pages']           = {
+                    'BOOK_LC_Githzerai_TornPage_01',
+                    'BOOK_LC_Githzerai_TornPage_02'
+                },
+                -- This message appears as a message box to inform the user what is going on
                 ['summonMessage']   = DEFAULT_SUMMON_MESAGE,
+                -- Upon combination of the pages, one of these summoning spells will be cast to summon your companion
                 ['summonSpells']    = {
                     {
                         ['name']       = 'LC_Summon_Githzerai_Legendary',
                         ['entityUUID'] = GTY_LEGENDARY_UUID
                     },
-                    --[[
-                    {
-                        ['name']       = 'LC_Summon_Githzerai_Rare',
-                        ['entityUUID'] = GTY_RARE_UUID
-                    },
-                    {
-                        ['name']       = 'LC_Summon_Githzerai_Common',
-                        ['entityUUID'] = GTY_COMMON_UUID
-                    },
-                    ]]
                 },
-                ['entityUUIDs']     = {
-                    GTY_LEGENDARY_UUID,
-                },
-                ['name']            = 'BOOK_LC_Githzerai_Combined_Tome_3',
-                ['pages']           = {
-                    'BOOK_LC_Githzerai_TornPage_01',
-                    'BOOK_LC_Githzerai_TornPage_02'
-                },
+                -- When your companion is summoned, it will cast one of these spells on the party
                 ['buffPartySpells'] = {
                     'Target_Bless_3_AI',
                 },
+                -- When your companion is summoned, one of these statuses will be applied to the companion
                 ['selfStatus']      = {
                     'WARMAGIC_GITHYANKI',
                 }
             }
-        },
-        [RARITY_COMMON] = {
-            ['entityUUIDs']     = {
-                GTY_COMMON_UUID,
-            },
-            ['buffPartySpells'] = {
-                'Target_PLA_Githyanki_Rally',
-            },
-            ['selfStatus']      = {
-                'WARMAGIC_GITHYANKI',
-            }
-        },
-        [RARITY_RARE] = {
-            ['entityUUIDs']     = {
-                GTY_RARE_UUID,
-            },
-            ['buffPartySpells'] = {
-                'Target_PLA_Githyanki_Rally',
-            },
-            ['selfStatus']      = {
-                'WARMAGIC_GITHYANKI',
-            }
-        },
-        [RARITY_LEGENDARY] = {
-            ['entityUUIDs']     = {
-                GTY_LEGENDARY_UUID,
-            },
-            ['buffPartySpells'] = {
-                'Target_PLA_Githyanki_Rally',
-            },
-            ['selfStatus']      = {
-                'WARMAGIC_GITHYANKI',
-            }
         }
     }
-}
+})
