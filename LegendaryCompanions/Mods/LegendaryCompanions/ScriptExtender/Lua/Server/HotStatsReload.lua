07fe42b60c9ba54650e6705cb3fd8bc2c2841b1d
Butterscotch Stallion <mistborn+github@fastmail.com>
2023-12-10 14:48:25 -0500

    Refactors separate summon files
    - Apparently if you have the base spell in a different
    file from a spell which inherits that spell, it will not work
    - Moves summons into a single file again as a result
    - Adds more equipment for LBB
    - Comments some files in hot reload because they were crashing the
    game

--- LegendaryCompanions/Mods/LegendaryCompanions/ScriptExtender/Lua/Server/HotStatsReload.lua
+++ LegendaryCompanions/Mods/LegendaryCompanions/ScriptExtender/Lua/Server/HotStatsReload.lua
@@ -1,21 +1,23 @@
 local MOD_PATH = 'Public/LegendaryCompanions/Stats/Generated/Data/'
 local FILES_TO_RELOAD = {
     'LC_Summons_Buffs.txt',
-    'LC_Summons.txt',
-    'LC_Summons_Githzerai.txt',
-    'LC_Summons_RSO.txt',
+    --'LC_Summons.txt',
+    --'LC_Summons_Githzerai.txt',
+    --'LC_Summons_RSO.txt',
     'LC_Books.txt',
 }
 
 local function OnReset()
     for _, filename in pairs(FILES_TO_RELOAD) do
-        local filePath = string.format('%s%s', MOD_PATH, filename)
-        if string.len(filename) > 0 then
-            LC['log'].Info(string.format('RELOADING %s', filePath))
-            ---@diagnostic disable-next-line: undefined-field
-            Ext.Stats.LoadStatsFile(filePath, false)
-        else
-            LC['log'].Critical(string.format('Invalid file: %s', filePath))
+        if filename then
+            local filePath = string.format('%s%s', MOD_PATH, filename)
+            if string.len(filename) > 0 then
+                LC['log'].Info(string.format('RELOADING %s', filePath))
+                ---@diagnostic disable-next-line: undefined-field
+                Ext.Stats.LoadStatsFile(filePath, false)
+            else
+                LC['log'].Critical(string.format('Invalid file: %s', filePath))
+            end
         end
     end
 end
