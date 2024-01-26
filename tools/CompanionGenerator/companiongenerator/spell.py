c779832e2629efb6ce16e6bbfe9f85451f11e07b
Butterscotch Stallion <mistborn+github@fastmail.com>
2024-01-25 22:40:55 -0500

    Updates template, removes commented code

--- tools/CompanionGenerator/companiongenerator/spell.py
+++ tools/CompanionGenerator/companiongenerator/spell.py
@@ -31,20 +31,5 @@ class SummonSpell(Spell, TemplateReplacerBase):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
         self.filename = "summon_spell.txt"
-        self.summon_uuid = kwargs["summon_uuid"]
         self.template_fetcher = kwargs["template_fetcher"]
-        self.replacements["{{summon_uuid}}"] = self.summon_uuid
-
-    """
-    def get_spell_template_text(self):
-        return self.template_fetcher.get_template_text("summon_spell.txt")
-
-    def get_spell_text(self):
-        spell_tpl = self.get_spell_template_text()
-        if spell_tpl:
-            replacer = TemplateVariableReplacer()
-            replaced_text = replacer.replace_placeholders(spell_tpl, self.replacements)
-            return replaced_text
-        else:
-            log.error("Error reading summon spell template")
-    """
+        self.replacements["{{summon_uuid}}"] = kwargs["summon_uuid"]
