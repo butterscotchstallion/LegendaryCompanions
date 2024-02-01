4ee7b11fe3d4a8a9d6d24a65ef0ddf08cc0f0608
Butterscotch Stallion <mistborn+github@fastmail.com>
2024-01-31 22:46:27 -0500

    Fixes logging formatting

--- tools/CompanionGenerator/companiongenerator/logger.py
+++ tools/CompanionGenerator/companiongenerator/logger.py
@@ -3,6 +3,6 @@ from loguru import logger
 logger.add(
     "../logs/LC-Automation.log",
     colorize=True,
-    format="<level>{time:YYYY-MM-DD at HH:mm:ss}{message}</level>",
+    format="<level>{time:YYYY-MM-DD at HH:mm:ss} {message}</level>",
     rotation="1 day",
 )
