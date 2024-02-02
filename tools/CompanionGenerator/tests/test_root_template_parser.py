82b5678cafa6a84426114948840e295e07834b95
Butterscotch Stallion <mistborn+github@fastmail.com>
2024-02-01 22:53:08 -0500

    Refactors to use merged file with actual contents

--- tools/CompanionGenerator/tests/test_root_template_parser.py
+++ tools/CompanionGenerator/tests/test_root_template_parser.py
@@ -6,6 +6,29 @@ from companiongenerator.root_template_parser import RootTemplateParser
 from tests.rt_test_helper import get_companion_rt, get_page_rt, get_scroll_rt
 
 
+def verify_node_children(node_children: list[ET.Element], root_template_object) -> bool:
+    """
+    Iterates node children and verifies one of them has the root
+    template object map key which is unique
+    """
+    for node_child in node_children:
+        """
+        Each node has a series of attribute tags
+        """
+        attrs = node_child.findall("attribute")
+        for attr in attrs:
+            # <attribute id="MapKey" type="FixedString" value="{{mapKey}}" />
+            is_map_key = attr.attrib["id"] == "MapKey"
+            has_value = "value" in attr.attrib
+            # Localization entries do not have a value
+            map_key_match = (
+                has_value and attr.attrib["value"] == root_template_object.map_key
+            )
+            if is_map_key and map_key_match:
+                return True
+    return False
+
+
 def test_parse_and_append():
     """
     Tests parsing XML files and appending to the
@@ -34,9 +57,9 @@ def test_parse_and_append():
         ),
     ]
 
-    # Parse XML and verify children tag exist
+    # Parse XML and verify children tag exists
     xml_with_new_nodes = parser.append_nodes_to_children(
-        "./companiongenerator/templates/merged.lsx",
+        "./companiongenerator/templates/merged_with_contents.lsf.lsx",
         new_nodes,
     )
     assert (
@@ -48,6 +71,12 @@ def test_parse_and_append():
     children_el = parser.get_templates_children(root)
     assert children_el is not None
 
+    """
+    Verify XML by checking each root template against
+    the parsed XML
+    """
     node_children = children_el.findall("node")
+    root_templates_to_verify = [companion_rt, page_rt, scroll_rt]
 
-    assert len(node_children) == len(new_nodes), "Failed to append to children"
+    for template in root_templates_to_verify:
+        assert verify_node_children(node_children, template)
