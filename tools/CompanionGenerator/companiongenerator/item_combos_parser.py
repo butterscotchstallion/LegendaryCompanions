3a253eac697acb394eeb4291f888c058a7b2b62e
Butterscotch Stallion <mistborn+github@fastmail.com>
2024-02-14 22:32:21 -0500

    Refactors ItemCombosParser
    - Refactors to add new method that parses combo names
    as well as combo result names
    - Refactors test to check that each combo name has
    an accompanying combo result

--- tools/CompanionGenerator/companiongenerator/item_combos_parser.py
+++ tools/CompanionGenerator/companiongenerator/item_combos_parser.py
@@ -1,6 +1,3 @@
-from pathlib import Path
-
-from companiongenerator.constants import MOD_FILENAMES
 from companiongenerator.stats_parser import StatsParser
 
 
@@ -10,20 +7,51 @@ class ItemCombosParser:
     def __init__(self):
         self.is_file_empty = False
 
-    def get_combo_names_from_file(self) -> list[str]:
-        handle = Path(MOD_FILENAMES["item_combos"])
-        file_contents = handle.read_text()
+    def get_combo_names_from_file_contents(self, file_contents: str) -> list[str]:
         combo_names: list[str] = []
 
         self.is_file_empty = not file_contents or len(file_contents) == 0
 
         if not self.is_file_empty:
-            combo_file_lines = [
-                line.strip() for line in file_contents.splitlines() if line.strip()
-            ]
+            combo_file_lines = self.get_combo_file_lines(file_contents)
             stats_parser = StatsParser()
             for combo_line in combo_file_lines:
                 if combo_line.startswith("new ItemCombination"):
                     combo_name = stats_parser.get_value_from_line_in_quotes(combo_line)
                     combo_names.append(combo_name)
         return combo_names
+
+    def get_combo_file_lines(self, file_contents: str):
+        return [line.strip() for line in file_contents.splitlines() if line.strip()]
+
+    def get_combo_entries_from_file_contents(
+        self, file_contents: str
+    ) -> dict[str, list[str]]:
+        """
+        Fetches both combo names and result names from the file contents
+        """
+        combo_names: list[str] = []
+        combo_result_names: list[str] = []
+        self.is_file_empty = not file_contents or bool(
+            file_contents and len(file_contents) == 0
+        )
+
+        if not self.is_file_empty:
+            combo_file_lines = self.get_combo_file_lines(file_contents)
+            stats_parser = StatsParser()
+            for combo_line in combo_file_lines:
+                """
+                We need a quote after the ItemCombination otherwise
+                we get result names too!
+                """
+                if combo_line.startswith('new ItemCombination "'):
+                    combo_name = stats_parser.get_value_from_line_in_quotes(combo_line)
+                    combo_names.append(combo_name)
+
+                if combo_line.startswith("new ItemCombinationResult"):
+                    combo_result_name = stats_parser.get_value_from_line_in_quotes(
+                        combo_line
+                    )
+                    combo_result_names.append(combo_result_name)
+
+        return {"combo_names": combo_names, "combo_result_names": combo_result_names}
--- tools/CompanionGenerator/tests/test_item_combos_parser.py
+++ tools/CompanionGenerator/tests/test_item_combos_parser.py
@@ -1,10 +1,29 @@
+from pathlib import Path
+
+from companiongenerator.constants import MOD_FILENAMES
 from companiongenerator.item_combos_parser import ItemCombosParser
 from companiongenerator.logger import logger
 
 
 def test_item_combos_parser():
     parser = ItemCombosParser()
-    combo_names = parser.get_combo_names_from_file()
+    handle = Path(MOD_FILENAMES["item_combos"])
+    file_contents = handle.read_text()
+
+    combo_entries = parser.get_combo_entries_from_file_contents(file_contents)
+    combo_names = combo_entries["combo_names"]
+    combo_result_names = combo_entries["combo_result_names"]
+
+    assert combo_names, "Empty combo names"
+    assert combo_result_names, "Empty combo result names"
+
+    # Test that each combo entry has an accompanying combo result
+    assert len(combo_names) == len(combo_result_names), "Unbalanced combo entries!"
+
+    for combo_name in combo_names:
+        assert (
+            f"{combo_name}_1" in combo_result_names
+        ), f"Missing combo result for {combo_name}!"
 
     if parser.is_file_empty:
         logger.error("Empty combo file")
